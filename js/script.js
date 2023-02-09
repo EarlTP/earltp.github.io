316a1e2bfa18b44592b047fef84eb71f65ba5c2e
EarlTP <earltp36@gmail.com>
2023-02-09 23:50:31 +0100

    init

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 jQuery(document).ready(() => {
-    const sakura = new Sakura('body');
+    const sakura = new Sakura('body', {
+      colors: [
+          {
+              gradientColorStart: 'rgba(98, 76, 171, 0.9)',
+              gradientColorEnd: 'rgba(218, 197, 221, 0.9)',
+              gradientColorDegree: 120
+          },
+          {
+              gradientColorStart: 'rgba(219, 144, 219, 0.9)',
+              gradientColorEnd: 'rgba(231, 160, 229, 0.9)',
+              gradientColorDegree: 120
+          },
+          {
+              gradientColorStart: 'rgba(98, 76, 171, 0.9)',
+            gradientColorEnd: 'rgba(218, 197, 221, 0.9)',
+            gradientColorDegree: 120
+          },
+      ],
+  });
 
     const config = {
         name: "Luca & Karen",
