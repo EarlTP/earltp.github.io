1110dfd5b98d05d3c46bbac6dfc741d7d32f631c
Luca Chiavelli <EarlTP@users.noreply.github.com>
2023-02-10 09:15:11 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -59,7 +59,7 @@ jQuery(document).ready(() => {
         window.atcb_action(eventConfig, event.currentTarget)
     });
     
-    jQuery('.map-link'.click((event) => {
+    jQuery('.map-link').click((event) => {
         const link = event.currentTarget;
         let address = encodeURIComponent(link.dataset.address);
         if ((navigator.platform.indexOf('iPhone') != -1) || (navigator.platform.indexOf('iPad') != -1) || (navigator.platform.indexOf('iPod') != -1)){/* if we're on iOS, open in Apple Maps */
